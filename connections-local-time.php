bad1ff40a4e74ffd6d4a29c5669d76d81dbd1d63
Steven A. Zahm <shazahm1@hotmail.com>
2021-05-04 10:20:23 -0400

    Remove use of `create_function()`.

--- connections-local-time.php
+++ connections-local-time.php
@@ -264,10 +264,9 @@ if ( ! class_exists( 'Connections_Local_Time' ) ) {
 
 			add_action(
 				'admin_notices',
-				create_function(
-					'',
-					'echo \'<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Local Time.</p></div>\';'
-				)
+				function() {
+					echo '<div id="message" class="error"><p><strong>ERROR:</strong> Connections must be installed and active in order use Connections Local Time.</p></div>';
+				}
 			);
 
 			return FALSE;
